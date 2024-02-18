bc4663f232a708926abe586067486bd28a2699c4
Antonina Cherenkova <everyrubbish@yandex.com>
2024-02-18 16:32:39 +0300

    добавила readme 1

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+
 from main import BooksCollector
 
 
@@ -21,4 +22,3 @@ def prepare_books(object):
     for book, genre in books_genre:
         object.add_new_book(book)
         object.set_book_genre(book, genre)
-    return books_genre
