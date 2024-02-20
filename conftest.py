afa6d09da3d8a0d1ea227b1dfbad967c88b3f518
Antonina Cherenkova <everyrubbish@yandex.com>
2024-02-20 20:53:32 +0300

    fix by comments

--- conftest.py
+++ conftest.py
@@ -8,15 +8,20 @@ def object():
     collector = BooksCollector()
     return collector
 
+fantasy_book = 'Что делать, если ваш кот хочет вас убить'
+horror_book = 'Гордость и предубеждение и зомби'
+detective_book = 'Шурик Хомлс и Воланд'
+child_book = 'Колобок'
+comedy_book = 'Три дома'
 
 @pytest.fixture(scope="function")
 def prepare_books(object):
     books_genre = [
-        ("Что делать, если ваш кот хочет вас убить", "Фантастика"),
-        ("Гордость и предубеждение и зомби", "Ужасы"),
-        ("Шурик Хомлс и Воланд", "Ужасы"),
-        ("Колобок", "Мультфильмы"),
-        ("Три дома", "Комедии"),
+        (fantasy_book, "Фантастика"),
+        (horror_book, "Ужасы"),
+        (detective_book, "Детективы"),
+        (child_book, "Мультфильмы"),
+        (comedy_book, "Комедии"),
     ]
 
     for book, genre in books_genre:
--- tests.py
+++ tests.py
@@ -1,16 +1,20 @@
 import pytest
 
+from conftest import fantasy_book, detective_book, child_book, comedy_book, horror_book
+
 
 class TestBooksCollector:
 
     #проверка добавления книг в список
-    def test_add_new_book_add_five_books_successfully(self, object, prepare_books):
-        assert len(object.get_books_genre()) == 5
+    def test_add_new_book_add_five_books_successfully(self, object):
+        object.add_new_book(fantasy_book)
+        object.set_book_genre(fantasy_book, 'Фантастика')
+        assert len(object.get_books_genre()) == 1
 
     #проверка добавления книг с одинаковым названием в список
     def test_add_new_book_add_similar_books(self, object):
-        object.add_new_book('Что делать, если ваш кот хочет вас убить')
-        object.add_new_book('Что делать, если ваш кот хочет вас убить')
+        object.add_new_book(fantasy_book)
+        object.add_new_book(fantasy_book)
 
         assert len(object.get_books_genre()) == 1
 
@@ -23,54 +27,48 @@ class TestBooksCollector:
 
     # проверка, что книги добавляются без жанра
     def test_add_new_book_add_books_without_genre(self, object):
-        name = 'Что делать, если ваш кот хочет вас убить'
-        object.add_new_book(name)
-
-        assert object.get_books_genre().get(name) == ''
+        object.add_new_book(fantasy_book)
+        assert object.get_books_genre().get(fantasy_book) == ''
 
     # проверка установления жанра книги
     def test_set_book_genre_get_genre_successfully(self, object, prepare_books):
-        name = 'Что делать, если ваш кот хочет вас убить'
-        assert object.get_book_genre(name) == 'Фантастика'
+        assert object.get_book_genre(fantasy_book) == 'Фантастика'
 
     # проверка вывода списка книг с определенным жанром
     def test_get_books_with_specific_genre_five_books_get_list_genre(self, object, prepare_books):
         genre = 'Ужасы'
-        assert object.get_books_with_specific_genre(genre) == ['Гордость и предубеждение и зомби', 'Шурик Хомлс и Воланд']
+        assert object.get_books_with_specific_genre(genre) == [horror_book]
 
     # проверка вывода списка книг, которые подходят детям
     def test_get_books_for_children_five_books_get_list_book(self, object, prepare_books):
-        assert object.get_books_for_children() == ['Что делать, если ваш кот хочет вас убить', 'Колобок', 'Три дома']
+        assert object.get_books_for_children() == [fantasy_book, child_book, comedy_book]
 
     #один тест с параметризацией, чтобы выполнить задание, однако я везде пользуюсь фикстурой prepare_books для удобства
     @pytest.mark.parametrize(
-        'book, genre',
+        'book, expected_result',
         [
-            ("Что делать, если ваш кот хочет вас убить", "Фантастика"),
-            ("Гордость и предубеждение и зомби", "Ужасы"),
-            ("Шурик Хомлс и Воланд", "Ужасы"),
-            ("Колобок", "Мультфильмы"),
-            ("Три дома", "Комедии"),
+            (fantasy_book, True),
+            (horror_book, False),
+            (detective_book, False),
+            (child_book, True),
+            (comedy_book, True),
         ]
     )
     # проверка что в списке книг для детей нет книг для взрослых
-    def test_get_books_for_children_adult_books_not_included_the_list(self, object, book, genre):
-        object.add_new_book(book)
-        object.set_book_genre(book, genre)
-
-        assert 'Гордость и предубеждение и зомби' not in object.get_books_for_children() and 'Шурик Хомлс и Воланд' not in object.get_books_for_children()
+    def test_get_books_for_children_adult_books_not_included_the_list(self, object, prepare_books, book, expected_result):
+        children_books = object.get_books_for_children()
+        assert (book in children_books) == expected_result
 
     # проверка добавления книги в избранное
     def test_add_book_in_favorites_add_one_books_successfully(self, object, prepare_books):
-        name = 'Колобок'
-        object.add_book_in_favorites(name)
+        object.add_book_in_favorites(fantasy_book)
 
-        assert object.get_list_of_favorites_books() == [name]
+        assert object.get_list_of_favorites_books() == [fantasy_book]
 
     # проверка, что нельзя добавить книгу дважды в избранное
     def test_add_book_in_favorites_same_books(self, object, prepare_books):
-        object.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
-        object.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
+        object.add_book_in_favorites(fantasy_book)
+        object.add_book_in_favorites(fantasy_book)
 
         assert len(object.get_list_of_favorites_books()) == 1
 
@@ -81,8 +79,7 @@ class TestBooksCollector:
 
     # проверка удаления книги из избранного
     def test_delete_book_from_favorites_removes_book_successfully(self, object, prepare_books):
-        name = 'Три дома'
-        object.add_book_in_favorites(name)
-        object.delete_book_from_favorites(name)
+        object.add_book_in_favorites(fantasy_book)
+        object.delete_book_from_favorites(fantasy_book)
 
         assert object.get_list_of_favorites_books() == []
